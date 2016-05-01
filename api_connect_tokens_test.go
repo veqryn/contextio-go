9e60a5e60964c032d98a3e0c539dab3f2c8050e6
Chris Duncan <veqryn@hotmail.com>
2016-05-01 00:33:28 -0600

    UN-176: more testing on connect tokens

--- api_connect_tokens_test.go
+++ api_connect_tokens_test.go
@@ -2,6 +2,7 @@ package ciolite
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -14,6 +15,7 @@ func TestActualConnectTokenRequestToCio(t *testing.T) {
 
 	cioLite, logger := NewTestCioLiteWithLogger(t)
 
+	// create
 	connectToken, err := cioLite.CreateConnectToken(CreateConnectTokenParams{
 		CallbackURL: "https://bogusurl.com",
 		Email:       "test@gmail.com",
@@ -23,6 +25,7 @@ func TestActualConnectTokenRequestToCio(t *testing.T) {
 		t.Error("Expected successful connect token; Got: ", connectToken, "; With Error: ", err, "; With Log: ", logger.String())
 	}
 
+	// get this one
 	getConnectToken, err := cioLite.GetConnectToken(connectToken.Token)
 
 	if err != nil ||
@@ -37,6 +40,7 @@ func TestActualConnectTokenRequestToCio(t *testing.T) {
 		t.Error("Expected GetConnectTokenResponse matching: ", connectToken, "; Got: ", getConnectToken, "; With Error: ", err, "; With Log: ", logger.String())
 	}
 
+	// get all
 	getConnectTokens, err := cioLite.GetConnectTokens()
 
 	found := false
@@ -50,6 +54,28 @@ func TestActualConnectTokenRequestToCio(t *testing.T) {
 		t.Error("Expected to include: ", getConnectToken, "; Got: ", getConnectTokens, "; With Log: ", logger.String())
 	}
 
+	// check with bad email
+	_, err = cioLite.CheckConnectToken("not_correct@gmail.com", connectToken.Token)
+	expectedErrorText := "Email does not match Context.io token"
+	if err.Error() != expectedErrorText {
+		t.Error("Expected error: ", expectedErrorText, "; Got: ", err)
+	}
+
+	// check with good email
+	_, err = cioLite.CheckConnectToken("test@gmail.com", connectToken.Token)
+	expectedErrorText = "Context.io token not used yet"
+	if err.Error() != expectedErrorText {
+		t.Error("Expected error: ", expectedErrorText, "; Got: ", err)
+	}
+
+	// Check if accepted token's email accounts match (they shouldn't since its not accepted)
+	_, err = getConnectToken.User.EmailAccountMatching("test@gmail.com")
+	expectedErrorText = "No email accounts match"
+	if !strings.HasPrefix(err.Error(), expectedErrorText) {
+		t.Error("Expected error prefix: ", expectedErrorText, "; Got: ", err)
+	}
+
+	// delete
 	deleteResponse, err := cioLite.DeleteConnectToken(connectToken.Token)
 
 	if err != nil || !deleteResponse.Success {
