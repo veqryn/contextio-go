e8092552ab1a6953a4bfd97758f24ea66fe64dba
Chris Duncan <veqryn@hotmail.com>
2016-04-30 23:06:47 -0600

    UN-176: unit test cio lite lib param methods

---
+++ params_test.go
@@ -0,0 +1,45 @@
+package ciolite
+
+import (
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+// TestFormValues tests that the form values function returns the correct url.Values
+func TestFormValuesAndQueryString(t *testing.T) {
+	t.Parallel()
+
+	params := struct {
+		StringFull  string `json:"string_full"`
+		StringEmpty string `json:"string_empty"`
+		BoolTrue    bool   `json:"bool_true"`
+		BoolFalse   bool   `json:"bool_false"`
+		IntLarge    int    `json:"int_large"`
+		IntZero     int    `json:"int_zero"`
+	}{
+		StringFull: "hello world",
+		BoolTrue:   true,
+		IntLarge:   8194723,
+	}
+
+	expectedFormValues := url.Values{
+		"string_full": []string{"hello world"},
+		"bool_true":   []string{"1"},
+		"int_large":   []string{"8194723"},
+	}
+
+	formValues := FormValues(params)
+
+	if !reflect.DeepEqual(formValues, expectedFormValues) {
+		t.Error("Expected form values: ", expectedFormValues, "; Got: ", formValues)
+	}
+
+	expectedQueryString := "?bool_true=1&int_large=8194723&string_full=hello+world"
+
+	queryString := QueryString(params)
+
+	if queryString != expectedQueryString {
+		t.Error("Expected query string: ", expectedQueryString, "; Got: ", queryString)
+	}
+}
